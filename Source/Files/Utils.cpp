3287d3d02b24256f8695485cd4da38c3e28ba699
Christian Hinkle <christianhinko@gmail.com>
2026-03-27 12:59:55 -0600

    [files][rename][cpp][cmake] Rename "DAM" source files to "Utils".

--- Source/Files/Utils.cpp
+++ Source/Files/Utils.cpp
@@ -1,6 +1,6 @@
 // Copyright (c) 2023-2025 Christian Hinkle, Brian Hinkle.
 
-#include <MeddySDK/DAM/DAM.h>
+#include <MeddySDK/DAM/Utils.h>
 
 #include <boost/filesystem/path.hpp>
 #include <boost/filesystem/operations.hpp>
